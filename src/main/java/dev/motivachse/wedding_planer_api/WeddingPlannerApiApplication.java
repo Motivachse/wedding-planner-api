9cb96553308f506963df1cc09a1ace2bfc5f1f58
ballaboidi <mario@autherith-media.eu>
2025-03-02 10:53:36 +0100

    first commit

---
+++ src/main/java/dev/motivachse/wedding_planer_api/WeddingPlannerApiApplication.java
@@ -0,0 +1,13 @@
+package dev.motivachse.wedding_planer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingPlannerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingPlannerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/motivachse/wedding_planer_api/WeddingPlannerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.motivachse.wedding_planer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingPlannerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
